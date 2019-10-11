397f54d1674ff7efb1c3a7b2edff7bc20e6201eb
Mike Hayes <mghayes93@gmail.com>
2019-10-10 17:51:44 -0700

    Creating local lists for players for testing

---
+++ game/Assets/Scripts/Entities/Entity.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ArenaGame
+{
+    public class Entity : MonoBehaviour
+    {
+        [SerializeField]
+        public string Name;
+        public long ID;
+        public int HP;
+        public int Strength;
+
+        [SerializeField]
+        TestPlayerRepository playerDatabase;
+
+        private GameObject target;
+
+        public void Awake()
+        {
+            ID = playerDatabase.GeneratePlayerID(Name);
+
+            LoadPlayerData(ID);
+            playerDatabase.CreateEntity(this);
+        }
+
+        public void Start()
+        {
+            if (!target)
+            {
+                target = GameObject.FindGameObjectWithTag("Enemy");
+                long tarID = target.GetComponent<Entity>().ID;
+
+                if (tarID == this.ID)
+                {
+                    target = GameObject.FindGameObjectWithTag("Player2");
+                    tarID = target.GetComponent<Entity>().ID;
+                }
+
+                Debug.Log(target.GetComponent<Entity>().ID);
+
+                Entity tar = target.GetComponent<Entity>();
+
+                Debug.Log("Enemy Str: " + tar.Strength);
+
+            }
+        }
+
+        private void LoadPlayerData(long ID)
+        {
+            
+
+        }
+    }
+}
---
+++ game/Assets/Scripts/Entities/TestPlayerRepository.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ArenaGame
+{
+    public class TestPlayerRepository : MonoBehaviour
+    {
+        public Dictionary<long, string> playerID = new Dictionary<long, string>();
+        public List<Entity> entities = new List<Entity>();
+
+        public long GeneratePlayerID(string name)
+        {
+            //long newPlayerID = (long)Random.Range(0, long.MaxValue);
+            long newPlayerID = 1;
+            if (playerID.ContainsKey(newPlayerID))
+            {
+                do
+                {
+                    //newPlayerID = (long)Random.Range(0, long.MaxValue);
+                    newPlayerID++;
+                } while (playerID.ContainsKey(newPlayerID));
+
+            }
+
+            // add the id / name to the dictionary
+            playerID.Add(newPlayerID, name);
+            Debug.Log("Player ID: " + newPlayerID + " / Player Name: " + name);
+
+            return newPlayerID;
+        }
+
+        public void CreateEntity(Entity player)
+        {
+            Entity newPlayer = GenerateStats(player);
+            entities.Add(newPlayer);
+
+            for (int i = 0; i < entities.Count; i++)
+            {
+                //Debug.Log(entities[i].HP);
+                //Debug.Log(entities[i].Strength);
+            }
+
+        }
+
+        public Entity GenerateStats(Entity player)
+        {
+            Entity newPlayer = player;
+            newPlayer.Strength = 10 + (int)player.ID;
+            newPlayer.HP = 100 + (int)player.ID;
+            
+            return newPlayer;
+        }
+    }
+}
